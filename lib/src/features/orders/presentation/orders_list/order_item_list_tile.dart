2aeefdc8d65bdf9a5da89991260ba37fa75ac505
Bishop <bishopuzochukwu37@gmail.com>
2022-09-13 10:45:51 +0100

    migrated code to use super initializers

--- lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
+++ lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
-import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_product_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
@@ -11,7 +10,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Shows an individual order item, including price and quantity.
 class OrderItemListTile extends ConsumerWidget {
-  const OrderItemListTile({Key? key, required this.item}) : super(key: key);
+  const OrderItemListTile({super.key, required this.item});
   final Item item;
 
   @override
